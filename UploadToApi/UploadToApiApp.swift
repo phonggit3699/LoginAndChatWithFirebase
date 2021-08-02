68d410e48670d9c1f6980c1733110b25bb6566ab
phonggit3699 <phamphong3699@gmail.com>
2021-08-02 07:45:25 +0700

    Initial Commit

---
+++ UploadToApi/UploadToApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UploadToApiApp.swift
+//  UploadToApi
+//
+//  Created by PHONG on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct UploadToApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
